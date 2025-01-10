a691c0ac5afb8325796611975a79529ba45d4787
sahithi2213 <sahithis415@gmail.com>
2025-01-10 14:22:11 +0530

    modified

--- functions.js
+++ functions.js
@@ -53,8 +53,56 @@
 // let x = add(5,5);
 // console.log(x);
 
-let x = (a,b) => a+b;
-console.log(x(10,30));
+// let x = (a,b) => a+b;
+// console.log(x(10,30));
 
-let y =(a,b)=> {return a + b};
-console.log(y(3,3));
+// let y =(a,b)=> {return a + b};
+// console.log(y(3,3));
+
+// function hof(a){
+//     return(a);
+// };
+//  let x = hof(function(){return "this is callback function"});
+//  console.log(x); 
+
+//  let b = function(){
+//     console.log("anonymus")
+//   };
+//  b();
+
+// function add(callback){
+//     return callback(5,3);
+// };
+// let x = add(functiona(a,b){return a+b});
+// console.log(x);
+
+// let users = ["chitti","ani","anil","sahithi"]
+// console.log(users[0]);
+// console.log(users[1]);
+// console.log(users[2]);
+// console.log(users[3]);
+// for(let i=0;i<users.lemgth;i++){
+//     console.log(users[i]);
+// }
+
+// let x = users.map((user)=>{
+//     return user;
+// });
+// console.log(x);
+// let y = users.forEach((user)=>{
+//     return (user);
+// });
+// console.log(y);
+
+// var a= 10;
+// let b=30;
+// function x(){
+//     var user = "sahithi";
+//     let company ="amazon";
+//     const sal = 80000;
+//     console.log(user);
+//     console.log(company);
+//     console.log(sal);
+//     console.log(a , b);
+// }
+// x();
