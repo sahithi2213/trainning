9c658044221b1f7ba4ac69fc740a939e4a6fe9de
sahithi2213 <sahithis415@gmail.com>
2025-01-22 10:33:17 +0530

    upadted 21

--- async.js
+++ async.js
@@ -14,25 +14,65 @@
 // console.log(p1);
 
 
-let p2 = new Promise(( resolve , reject)=> {
-     resolve("success");
-});
-//console.log(p2);
-p2.then((response) =>
-{
-        console.log(response);
-}).catch((error)=>{
-        console.log(error);
-}).finally(()=>console.log("finally printing for both"))
+// let p2 = new Promise(( resolve , reject)=> {
+//      resolve("success");
+// });
+// //console.log(p2);
+// p2.then((response) =>
+// {
+//         console.log(response);
+// }).catch((error)=>{
+//         console.log(error);
+// }).finally(()=>console.log("finally printing for both"))
 
-let p3 = new Promise(( resolve , reject)=>{
-         reject("failures");
- });
- //console.log(p3);
-p3.then((response) =>
-{
-        console.log(response);
-}).catch((error)=>{
-        console.log(error);
-}).finally(()=>console.log("finally printing for both"))
+// let p3 = new Promise(( resolve , reject)=>{
+//          reject("failures");
+//  });
+//  //console.log(p3);
+// p3.then((response) =>
+// {
+//         console.log(response);
+// }).catch((error)=>{
+//         console.log(error);
+// }).finally(()=>console.log("finally printing for both"))
 
+
+// function fetchUser()
+// {
+//  let res = window.fetch("https://api.github.com/users");
+// //  console.log(res);
+// res.then(val =>{
+//         // console.log(val);
+//      //colsole.log(val.json());
+//      val.json().then(data)
+// })
+// }
+//   fetchUser();
+
+function fetchUsers(){
+        // let res = window.fetch("https://api.github.com/users");
+        let res = window.fetch("https://jsonplaceholder.typicode.com/users");
+        // console.log(res);
+        res.then((response)=>{
+            // console.log(response);
+            // console.log(response.json());
+            response.json().then(data=>{
+                console.log(data);
+                let store=document.getElementById("store");
+                // console.log(store);
+                data.map((user)=>{
+                    // console.log(user);
+                    store.innerHTML += `
+                    <tr>
+                    <td>${user.id}</td>
+                    <td>${user.name}</td>
+                    <td>${user.email}</td>
+                    <td>${user.company.name}</td>
+                    </tr>
+                    ` 
+                })
+            })
+        })
+        .catch(err=>console.log(err))
+    }
+    fetchUsers()
