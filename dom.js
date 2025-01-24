0af4b874c7353fabea96d7444828ebd7a8a097a9
sahithi2213 <sahithis415@gmail.com>
2025-01-24 14:03:14 +0530

    updated

--- dom.js
+++ dom.js
@@ -1,11 +1,54 @@
- // !document.getElementByID("");
+ // let element = document.getElementById("demo");
+// element.innnerText="DOM";
+// console.log(element);
 
-let element = document .getElementByID("demo");
-element.innerText = "DOM";
-console.log(element);
+// let div=document.getElementById("test");
+// div.innerHTML="<h1>Header</h1>";
+// console.log(div);
 
-let div = document.getElementByID("test");
-// div.innerText ="<h1>Header</h1>"
-div.innerHTML ="<h1>Header</h1>"
-console.log(div);
+// let ele=document.getElementsByClassName("test");
+// // console.log(ele);
+// // console.log(Array.isArray(ele));
+// ele[0].style.backgroundColor="yellow";
 
+// //spread operator
+// let x=[...ele];
+// console.log(x, Array.isArray(x));
+// x.map((element)=>{
+//     // console.log(element);
+//     element.style.backgroundColor="teal";
+// })
+
+// let ele=document.getElementsByTagName("div");
+// console.log(ele);
+
+// let ele=document.querySelector("#demo");
+// console.log(ele);
+
+// let ele=document.querySelectorAll(".test");
+// // console.log(ele);
+// [...ele].map((element)=>{
+//     console.log(element.innerText);
+// })
+
+
+// let btn = document.getElementById("btn");
+// btn.addEventListener("mouseover" , ()=>{
+//     // alert("mouse over done")
+//     document.body.style.background="teal"
+// })
+
+// btn.addEventListener("mouseleave" , ()=>{
+//     // alert("mouse leave done")
+//     document.body.style.background="tomato"
+// })
+
+
+// let inp = document.querySelector("#inp");
+// inp.addEventListener("keydown",()=>{
+//     console.log("key down");
+// })
+
+// inp.addEventListener("keyup",()=>{
+//     console.log("key up");
+// })
